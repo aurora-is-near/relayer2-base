7101768d0156710e501eabdba34b41a9cf789908
Altug Keyder <7550161+akeyder@users.noreply.github.com>
2022-10-14 15:00:06 +0300

    (#11) geth middleware

--- rpcnode/github-ethereum-go-ethereum/rpcnode.go
+++ rpcnode/github-ethereum-go-ethereum/rpcnode.go
@@ -19,9 +19,9 @@ package github_ethereum_go_ethereum
 import (
 	"aurora-relayer-go-common/broker"
 	eventbroker "aurora-relayer-go-common/rpcnode/github-ethereum-go-ethereum/events"
-
 	"github.com/ethereum/go-ethereum/node"
 	"github.com/jinzhu/copier"
+	"net/http"
 )
 
 // GoEthereum is a container on which underlying go-ethereum services can be registered.
@@ -56,9 +56,20 @@ func NewWithConf(conf *Config) (*GoEthereum, error) {
 	}, nil
 }
 
+// WithMiddleware places the given middleware at the beginning of HTTP handlers chain. A middleware is a function accepting
+// http.Handler and returning http.Handler. Any request matching the path argument is first processed by this middleware.
+// Middleware should either return response for the request or pass the request to next HTTP handler in the chain.
+// Refer to http.HandlerFunc for middleware creation.
+func (ge GoEthereum) WithMiddleware(name string, path string, middleware func(handler http.Handler) http.Handler) {
+	h, err := ge.RPCHandler()
+	if err != nil {
+		panic(err)
+	}
+	ge.RegisterHandler(name, path, middleware(h))
+}
+
 func convertConfigurationToEthNode(confAurora *Config) *node.Config {
 	confEth := &node.Config{}
 	copier.CopyWithOption(confEth, confAurora, copier.Option{IgnoreEmpty: true, DeepCopy: true})
-
 	return confEth
 }
