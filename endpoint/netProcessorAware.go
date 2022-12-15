e9947453c53267f11523f54f5b6904f2ec0fc55f
Altug Keyder <7550161+akeyder@users.noreply.github.com>
2022-12-15 12:07:53 +0300

    (#52) fix endpoint names

--- endpoint/netProcessorAware.go
+++ endpoint/netProcessorAware.go
@@ -19,7 +19,7 @@ func (e *NetProcessorAware) Listening(ctx context.Context) (*bool, error) {
 }
 
 func (e *NetProcessorAware) PeerCount(ctx context.Context) (*string, error) {
-	return Process(ctx, "net_listening", e.Endpoint, func(ctx context.Context) (*string, error) {
+	return Process(ctx, "net_peerCount", e.Endpoint, func(ctx context.Context) (*string, error) {
 		return e.Net.PeerCount(ctx)
 	})
 }
