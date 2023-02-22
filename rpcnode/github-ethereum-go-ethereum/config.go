0fab125a93cfceadd59e729774706e963c6b713e
Tolga Coplu <tolga.coplu@aurora.dev>
2023-02-22 11:57:05 +0300

    Remove the misleading copyright info

--- rpcnode/github-ethereum-go-ethereum/config.go
+++ rpcnode/github-ethereum-go-ethereum/config.go
@@ -1,25 +1,10 @@
-// Copyright 2014 The go-ethereum Authors
-// This file is part of the go-ethereum library.
-//
-// The go-ethereum library is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// The go-ethereum library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
-
 package github_ethereum_go_ethereum
 
 import (
+	"time"
+
 	"github.com/aurora-is-near/relayer2-base/cmd"
 	"github.com/aurora-is-near/relayer2-base/log"
-	"time"
 
 	gel "github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/rpc"
