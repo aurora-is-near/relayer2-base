e461d4535278e1b72e2ef190e45caf9634692123
Altug Keyder <7550161+akeyder@users.noreply.github.com>
2023-04-07 16:09:48 +0300

    (#100) provide lib for creating and serving metrics

---
+++ probe/config.go
@@ -0,0 +1,64 @@
+package probe
+
+import (
+	"github.com/aurora-is-near/relayer2-base/cmd"
+	"github.com/aurora-is-near/relayer2-base/log"
+	"github.com/spf13/viper"
+)
+
+const (
+	defaultAddress   = "127.0.0.1:28080"
+	defaultNamespace = "Aurora"
+	defaultSubsystem = "Relayer2"
+	defaultEnabled   = false
+
+	configPath = "probe"
+)
+
+type ServerConfig struct {
+	Address   string `mapstructure:"address"`
+	Namespace string `mapstructure:"namespace"`
+	Subsystem string `mapstructure:"subsystem"`
+}
+
+type MetricConfig struct {
+	Id          string    `mapstructure:"id"`
+	Type        string    `mapstructure:"type"`
+	Name        string    `mapstructure:"name"`
+	Help        string    `mapstructure:"help"`
+	Buckets     []float64 `mapstructure:"buckets"`
+	LabelNames  []string  `mapstructure:"labelNames"`
+	LabelValues []string  `mapstructure:"labelValues"`
+}
+
+type Config struct {
+	Enabled       bool            `mapstructure:"enable"`
+	ServerConfig  *ServerConfig   `mapstructure:"server"`
+	MetricConfigs *[]MetricConfig `mapstructure:"metrics"`
+}
+
+func defaultConfig() *Config {
+	return &Config{
+		Enabled: defaultEnabled,
+		ServerConfig: &ServerConfig{
+			Address:   defaultAddress,
+			Namespace: defaultNamespace,
+			Subsystem: defaultSubsystem,
+		},
+		MetricConfigs: &[]MetricConfig{},
+	}
+}
+
+func GetConfig() *Config {
+	config := defaultConfig()
+	sub := viper.Sub(configPath)
+	if sub != nil {
+		cmd.BindSubViper(sub, configPath)
+		if err := sub.Unmarshal(&config); err != nil {
+			log.Log().Warn().Err(err).Msgf("failed to parse configuration [%s] from [%s], "+
+				"falling back to defaults", configPath, viper.ConfigFileUsed())
+		}
+	}
+
+	return config
+}
