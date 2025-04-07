b8fd38faa87c93097e0e1076e80c901d4d2f4c38
Luis Carvalho <luis.carvalho@aurora.dev>
2025-04-07 10:04:36 +0100

    chore: use mutex in aurora RPC client

--- endpoint/aurora.go
+++ endpoint/aurora.go
@@ -3,6 +3,7 @@ package endpoint
 import (
 	"errors"
 	"net/http"
+	"sync"
 	"time"
 
 	"github.com/aurora-is-near/relayer2-base/log"
@@ -31,6 +32,7 @@ type AuroraRPC struct {
 	maxPriorityFeePerGasCache      *primitives.Quantity
 	maxPriorityFeePerGasValidUntil time.Time
 	url                            string
+	mu                             sync.Mutex
 }
 
 func NewAuroraRPC(url string) *AuroraRPC {
@@ -39,6 +41,9 @@ func NewAuroraRPC(url string) *AuroraRPC {
 
 func (a *AuroraRPC) MaxPriorityFeePerGas() (*primitives.Quantity, error) {
 	now := time.Now()
+
+	a.mu.Lock()
+	defer a.mu.Unlock()
 
 	if a.maxPriorityFeePerGasCache != nil && a.maxPriorityFeePerGasValidUntil.Before(now) {
 		return a.maxPriorityFeePerGasCache, nil
